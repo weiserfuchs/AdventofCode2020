4f5faaa38367bce1c11a0511956d847a297891c3
FabianW <weiserfuchs@gmx.net>
2020-12-05 18:16:43 +0100

    part 2

--- aoc_04.py
+++ aoc_04.py
@@ -17,36 +17,41 @@ with open('aoc_04_input.txt') as fd:
 
 iCounter = 0
 for passport in input:
-    if len(passport) == 8 or (len(passport) == 7 and "cid" not in passport):
+    if "byr" in passport and "iyr" in passport and "eyr" in passport and "hgt" in passport and "hcl" in passport and "ecl" in passport and "pid" in passport:
         bValid = True
         for field in passport:
             if bValid:
                 if field == "byr":
-                    bValid = (int(passport[field]) >= 1920 and int(passport[field]) <= 2002)
+                    if not (int(passport[field]) >= 1920 and int(passport[field]) <= 2002):
+                        bValid = False
                 elif field == "iyr":
-                    bValid = (int(passport[field]) >= 2010 and int(passport[field]) <= 2020)
+                    if not (int(passport[field]) >= 2010 and int(passport[field]) <= 2020):
+                        bValid = False
                 elif field == "eyr":
-                    bValid = (int(passport[field]) >= 2020 and int(passport[field]) <= 2030)
+                    if not (int(passport[field]) >= 2020 and int(passport[field]) <= 2030):
+                        bValid = False
                 elif field == "hgt":
                     tmp = passport[field]
                     if tmp[-2:] == "cm":
-                        bValid = (len(tmp) == 5 and int(tmp[:3]) >= 150 and int(tmp[:3]) <= 193)
+                        if not (len(tmp) == 5 and int(tmp[:3]) >= 150 and int(tmp[:3]) <= 193):
+                            bValid = False
                     elif tmp[-2:] == "in":
-                        bValid = (len(tmp) == 4 and int(tmp[:2]) >= 59 and int(tmp[:2]) <= 76)
+                        if not (len(tmp) == 4 and int(tmp[:2]) >= 59 and int(tmp[:2]) <= 76):
+                            bValid = False
+                    else:
+                        bValid = False
                 elif field == "hcl":
                     if not re.search("#[0-9a-f]{6}",passport[field]):
                         bValid = False
                 elif field == "ecl":
-                    bValid = (passport[field] in ["amb","blu","brn","gry","grn","hzl","oth"])
+                    if not (passport[field] in ["amb","blu","brn","gry","grn","hzl","oth"]):
+                        bValid = False
                 elif field == "pid":
-                    if not re.search("\d{9}",passport[field]):
+                    if not re.search("^\d{9}$",passport[field]):
                         bValid = False
                 else:
                     pass
-                    #print(passport[field])
         if bValid:
-            if re.search("#[0-9a-f]{6}",passport["hcl"]):
-                print(False)
             iCounter += 1
 
 
