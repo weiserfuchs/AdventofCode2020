793935a44641eabd433e2997da57d24c8c64c7a3
FabianW <weiserfuchs@gmx.net>
2020-12-07 20:50:37 +0100

    part 2

--- aoc_06.py
+++ aoc_06.py
@@ -5,17 +5,26 @@ with open('aoc_06_input.txt') as fd:
         input.append(line.strip())
 
 iTotal = 0
-answered = []
-for k in input:
+answered = {}
+iGrp = 0
+for i in range(len(input)):
     
-    if len(k) > 0:
-        for c in k:
-            if c not in answered:
-                answered.append(c)
-
+    if len(input[i]) > 0:
+        iGrp += 1
+        for c in input[i]:
+            if c in answered:
+                answered[c] += 1
+            else:
+                answered[c] = 1
+        if i == (len(input)-1):
+            for l in answered:
+                if answered[l] == iGrp:
+                    iTotal += 1
     else:
-        iTotal += len(answered)
-        answered = []
-iTotal += len(answered)
+        for l in answered:
+            if answered[l] == iGrp:
+                iTotal += 1
+        iGrp = 0
+        answered = {}
 
 print(iTotal)
